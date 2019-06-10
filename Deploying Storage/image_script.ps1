8fae2d115b0e8d575933e707a747f409b290ade4
elnino27 <33740885+elnino27@users.noreply.github.com>
2019-06-11 01:02:12 +0200

    Add files via upload

---
+++ Deploying Storage/image_script.ps1
@@ -0,0 +1,13 @@
+﻿ #### Script for converting into image ####
+
+ Add-AzureRmAccount
+ Get-AzureRmSubscription
+ $vmName = "SimplewinVM"
+ $rgName = "firstdeploy"
+ $location = "West Europe"
+ $imageName = "simplevm_image"
+ Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName -Force
+ Set-AzureRmVM -ResourceGroupName  $rgName -Name $vmName -Generalized
+ $vm = Get-AzureRmVM -ResourceGroupName  $rgName -Name $vmName
+ $image=New-AzureRmImageConfig -Location $location -SourceVirtualMachineId $vm.Id
+ New-AzureRmImage -Image $image -ImageName $imageName -ResourceGroupName $rgName
